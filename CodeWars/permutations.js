53b3314462a8b317a2ed199948ffe6c6f819a2aa
Simon <aguilarsimon01@gmail.com>
2020-02-15 21:29:56 -0300

    update codewars

---
+++ CodeWars/permutations.js
@@ -0,0 +1,44 @@
+// In this kata you have to create all permutations of an input string and remove duplicates, 
+// if present. This means, you have to shuffle all letters from the input in all possible orders.
+// The order of the permutations doesn't matter.
+
+function permutations(string) {
+
+
+      return string;
+    }
+    
+    const finalPermutations = [];
+    
+    //  // push to array. 
+    //  var str = string;
+    //  // Iterate through the string length.
+    //   for(let i = 0; i < finalPermutations.length; i++){
+    //    var start = a.indexOf(b);
+    //    var end = start + b.length;
+    
+    //   let = str.slice(-2)
+    //   finalPermutations.push(-2)
+    //   str.slice(-1)
+    //   finalPermutations.push(-1)
+    //   str.slice(-3)
+    //   finalPermutations.push(-3)
+    //   str.slice(-2)
+    //   finalPermutations.push(-2)
+    //  // Take the penultimum letter to the one next it.
+    // }
+    //  return finalPermutations;
+    //  // Finished the loop. Return an array with the permutations.
+    
+    
+    function takeOut(string){
+      return string;  
+    }
+    
+    console.log(takeOut('aabb'))
+    
+    
+    
+    // console.log(permutations('a')); //['a']
+    // console.log(permutations('ab')); //['ab', 'ba']
+    // console.log(permutations('aabb')); //['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
